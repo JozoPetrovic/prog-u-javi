ba8083bc59f42f7b10217a1eeaa323c1daf94e5e
JozoPetrovic <107885928+JozoPetrovic@users.noreply.github.com>
2025-01-22 23:51:52 +0100

    Add files via upload

---
+++ src/main/java/ba/sum/fpmoz/mymath/repository/UsersRepository.java
@@ -0,0 +1,8 @@
+package ba.sum.fpmoz.mymath.repository;
+
+import ba.sum.fpmoz.mymath.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository <User, Long> {
+    User findByEmail(String email);
+}
